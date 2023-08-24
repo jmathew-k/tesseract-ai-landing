6678fe6a6f32a1b2eb9bcbe398da9b532522ce0d
Joshua Mathew <jm@Joshuas-iMac.local>
2023-08-24 15:07:12 +0530

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,13 +25,13 @@ const grotesk = Space_Grotesk({
 });
 
 export const metadata = {
-    title: "Tesseract - LLMs for Analytics",
-    description:
-        "Tesseract uses Large Language Models to take user queries as natural language questions and then sifts through data streams to surface concise signals.",
-    icons: {
-        icon: "/favicon.ico",
-    },
-};
+	title: 'Tesseract- zkSHIELD',
+	description:
+		'Tesseract uses Large Language Models to take user queries as natural language questions and then sifts through data streams to surface concise signals.',
+	icons: {
+		icon: '/favicon.ico',
+	},
+}
 
 export default function RootLayout({
     children,
@@ -39,105 +39,72 @@ export default function RootLayout({
     children: React.ReactNode;
 }) {
     return (
-        <html lang="en">
-            <head>
-                <meta
-                    content="Tesseract uses Large Language Models to take user queries as natural language questions and then sifts through data streams to surface concise signals."
-                    name="Tesseract - LLMs for Research & Data"
-                />
-                <meta
-                    content="Tesseract - LLMs for Crypto Research & Data"
-                    property="og:title"
-                />
-                <meta
-                    content="Tesseract - LLMs for Crypto Research & Data"
-                    property="og:description"
-                />
-                <meta
-                    content="%PUBLIC_URL%/fb-og-image.png"
-                    property="og:image"
-                />
-                <meta
-                    property="og:url"
-                    content="https://tesseractxyz.com"
-                />
-                <meta
-                    property="og:site_name"
-                    content="Tesseract - LLMs for Crypto Research & Data"
-                />
-                <meta
-                    content="Tesseract - LLMs for Crypto Research & Data"
-                    property="twitter:title"
-                />
-                <meta
-                    content="Tesseract - LLMs for Crypto Research & Data"
-                    property="twitter:description"
-                />
-                <meta
-                    content="%PUBLIC_URL%/twitter-card.png"
-                    property="twitter:image"
-                />
-                <meta property="og:type" content="Article" />
-                <meta content="summary" name="twitter:card" />
-                <meta name="twitter:site" content="@tesseract_xyz" />
-                <meta name="twitter:creator" content="@tesseract_xyz" />
-                <meta property="fb:admins" content="132951670226590" />
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1, maximum-scale=1"
-                />
-                <meta name="msapplication-TileColor" content="#da532c" />
-                <meta name="theme-color" content="#ffffff" />
-            </head>
-            <body
-                className={`${sora.variable} ${code.variable} ${grotesk.variable} font-sans bg-n-8 text-n-1 text-base`}
-            >
-                {children}
-                <svg className="block" width={0} height={0}>
-                    <defs>
-                        <linearGradient
-                            id="btn-left"
-                            x1="50%"
-                            x2="50%"
-                            y1="0%"
-                            y2="100%"
-                        >
-                            <stop offset="0%" stopColor="#89F9E8" />
-                            <stop offset="100%" stopColor="#FACB7B" />
-                        </linearGradient>
-                        <linearGradient
-                            id="btn-top"
-                            x1="100%"
-                            x2="0%"
-                            y1="50%"
-                            y2="50%"
-                        >
-                            <stop offset="0%" stopColor="#D87CEE" />
-                            <stop offset="100%" stopColor="#FACB7B" />
-                        </linearGradient>
-                        <linearGradient
-                            id="btn-bottom"
-                            x1="100%"
-                            x2="0%"
-                            y1="50%"
-                            y2="50%"
-                        >
-                            <stop offset="0%" stopColor="#9099FC" />
-                            <stop offset="100%" stopColor="#89F9E8" />
-                        </linearGradient>
-                        <linearGradient
-                            id="btn-right"
-                            x1="14.635%"
-                            x2="14.635%"
-                            y1="0%"
-                            y2="100%"
-                        >
-                            <stop offset="0%" stopColor="#9099FC" />
-                            <stop offset="100%" stopColor="#D87CEE" />
-                        </linearGradient>
-                    </defs>
-                </svg>
-            </body>
-        </html>
-    );
+			<html lang='en'>
+				<head>
+					<meta
+						content='Tesseract uses Zero Knowledge Cryptography to make Web3 Apps Safer and Enhance Privacy'
+						name='Tesseract- zkSHIELD'
+					/>
+					<meta content='Tesseract- zkSHIELD' property='og:title' />
+					<meta content='Tesseract- zkSHIELD' property='og:description' />
+					<meta content='%PUBLIC_URL%/fb-og-image.png' property='og:image' />
+					<meta property='og:url' content='https://tesseractxyz.com' />
+					<meta property='og:site_name' content='Tesseract- zkSHIELD' />
+					<meta content='Tesseract- zkSHIELD' property='twitter:title' />
+					<meta content='Tesseract- zkSHIELD' property='twitter:description' />
+					<meta
+						content='%PUBLIC_URL%/twitter-card.png'
+						property='twitter:image'
+					/>
+					<meta property='og:type' content='Article' />
+					<meta content='summary' name='twitter:card' />
+					<meta name='twitter:site' content='@tesseract_xyz' />
+					<meta name='twitter:creator' content='@tesseract_xyz' />
+					<meta property='fb:admins' content='132951670226590' />
+					<meta
+						name='viewport'
+						content='width=device-width, initial-scale=1, maximum-scale=1'
+					/>
+					<meta name='msapplication-TileColor' content='#da532c' />
+					<meta name='theme-color' content='#ffffff' />
+				</head>
+				<body
+					className={`${sora.variable} ${code.variable} ${grotesk.variable} font-sans bg-n-8 text-n-1 text-base`}
+				>
+					{children}
+					<svg className='block' width={0} height={0}>
+						<defs>
+							<linearGradient id='btn-left' x1='50%' x2='50%' y1='0%' y2='100%'>
+								<stop offset='0%' stopColor='#89F9E8' />
+								<stop offset='100%' stopColor='#FACB7B' />
+							</linearGradient>
+							<linearGradient id='btn-top' x1='100%' x2='0%' y1='50%' y2='50%'>
+								<stop offset='0%' stopColor='#D87CEE' />
+								<stop offset='100%' stopColor='#FACB7B' />
+							</linearGradient>
+							<linearGradient
+								id='btn-bottom'
+								x1='100%'
+								x2='0%'
+								y1='50%'
+								y2='50%'
+							>
+								<stop offset='0%' stopColor='#9099FC' />
+								<stop offset='100%' stopColor='#89F9E8' />
+							</linearGradient>
+							<linearGradient
+								id='btn-right'
+								x1='14.635%'
+								x2='14.635%'
+								y1='0%'
+								y2='100%'
+							>
+								<stop offset='0%' stopColor='#9099FC' />
+								<stop offset='100%' stopColor='#D87CEE' />
+							</linearGradient>
+						</defs>
+					</svg>
+				</body>
+			</html>
+		)
 }
--- templates/HomePage/Hero/index.tsx
+++ templates/HomePage/Hero/index.tsx
@@ -28,9 +28,9 @@ const Hero = ({}: HeroProps) => {
             <div className="container relative" ref={parallaxRef}>
                 <div className="relative z-1 max-w-[62rem] mx-auto mb-[3.875rem] text-center md:mb-20 lg:mb-[6.25rem]">
                     <h1 className="h1 mb-6">
-                        New&nbsp;Paradigm&nbsp;for {" "}
+                        Zero Knowledge&nbsp;SHIELD&nbsp;for {" "}
                         <span className="inline-block relative">
-                            Ontology & Analytics
+                            DeFi
                             <Image
                                 className="absolute top-full left-0 w-full xl:-mt-2"
                                 src="/images/curve.png"
